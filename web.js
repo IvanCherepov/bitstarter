3ed73ac01300a3148d21d7c14c592d01141f50fb
Ivan Cherepov <cherapau@gmail.com>
2013-07-13 20:17:59 +0000

    Updated web.js, added index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+
+buffer.write(fs.readFileSync('./index.thml','utf8'));
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
+ {
 });
 
 var port = process.env.PORT || 5000;
