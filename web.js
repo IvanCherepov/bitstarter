d3023cc65d4d3d99fb98b031a2c2134eaef926a9
Ivan Cherepov <cherapau@gmail.com>
2013-07-13 20:43:15 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,14 @@
 var express = require('express');
-
-var app = express.createServer(express.logger());
-
 var fs = require('fs');
 
-var data = fs.readFileSync("index.thml","utf-8");
+var data = fs.readFileSync("index.html", "utf-8");
 
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(data); 
+  response.send(data);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
