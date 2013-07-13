1ee5523714ac9cb2b421f8933d9d85540677b26f
Ivan Cherepov <cherapau@gmail.com>
2013-07-13 20:31:36 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,13 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var buffer = new Buffer(8);
 
-buffer.write(fs.readFileSync('./index.thml','utf8'));
+buffer.write(fs.readFileSync('index.thml','utf8'));
 
+var data = fs.readFileSync('index.thml','utf8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(data);
  {
 });
 
