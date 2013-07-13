7097f955cd16781bb1b527114cb4a866ff0bf7e3
Ivan Cherepov <cherapau@gmail.com>
2013-07-13 20:34:35 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,15 +4,10 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-var buffer = new Buffer(8);
-
-buffer.write(fs.readFileSync('index.thml','utf8'));
-
 var data = fs.readFileSync('index.thml','utf8');
 
 app.get('/', function(request, response) {
-  response.send(data);
- {
+  response.send(data); {
 });
 
 var port = process.env.PORT || 5000;
